5c925e2cf8452a6af6201bd7ba95b2ddd163fe5e
PhoneBiz Developer <dev@phonebiz.com>
2026-05-22 04:36:35 +0800

    chore: 删除旧项目phonebiz残留文件, nginx改为新项目路径

--- data/apps/phonebiz/src/main/java/com/cmbchina/phonebiz/mapper/SysOrgMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cmbchina.phonebiz.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cmbchina.phonebiz.entity.SysOrg;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysOrgMapper extends BaseMapper<SysOrg> {
-}
